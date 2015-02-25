d841a3f768541c6ad24bec918c1bbffd6eaa05c4
Jon Egerton <jon@ja2.co.uk>
2015-02-25 17:09:32 +0000

    Commit scripts

---
+++ cf-ddns-read.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Reads all dns records for a cloudflare hosted domain.
+# Use this to get the domain id required for cloudflare ddns updates 
+# (is rec_id in the saved json at ~/.cf-dns.txt)
+#
+# Created 25-Feb-2015 Jon Egerton, from the rec_load_all documentation here: 
+# https://www.cloudflare.com/docs/client-api.html
+#
+# This version is working as at 25-Feb-2015
+# As/When cloudflare change their API amendments may be required
+
+cfkey=API_KEY_HERE
+cfuser=EMAIL_HERE
+domain=DOMAIN_HERE
+
+curl https://www.cloudflare.com/api_json.html \
+  -d a=rec_load_all \
+  -d tkn=$cfkey \
+  -d email=$cfuser \
+  -d z=$domain > $HOME/.cf-dns.txt
---
+++ cf-ddns.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# modified by jfro from http://www.cnysupport.com/index.php/linode-dynamic-dns-ddn$
+# Update: changed because the old IP-service wasn't working anymore
+# Uses curl to be compatible with machines that don't have wget by default
+# modified by Ross Hosman for use with cloudflare.
+# modified by Jon Egerton to add logging
+#
+# This version is working as at 25-Feb-2015
+# As/When cloudflare change their API amendments may be required
+#
+# Place at:
+# /usr/local/bin/cf-ddns.sh
+# run `crontab -e` and add next line:
+# /5 * * * * bash /usr/local/bin/cf-ddns.sh >/dev/null 2>&1
+
+cfkey=API_KEY_HERE
+cfuser=EMAIL_HERE
+cfhost=HOST_HERE (this is the specific home/subdomain line to be updated)
+cfid=API_ID_HERE (see separate script to get this)
+domain=DOMAIN_HERE
+
+log=$HOME/.cf-dns-update.log
+date +"%F %T" >> $log
+
+WAN_IP=`curl -s http://icanhazip.com`
+if [ -f $HOME/.wan_ip-cf.txt ]; then
+        OLD_WAN_IP=`cat $HOME/.wan_ip-cf.txt`
+else
+        echo "No file, need IP" >> $log
+        OLD_WAN_IP=""
+fi
+
+if [ "$WAN_IP" = "$OLD_WAN_IP" ]; then
+        echo  "IP Unchanged" >> $log
+else
+        echo $WAN_IP > $HOME/.wan_ip-cf.txt
+        echo "Updating DNS to $WAN_IP" >> $log
+         curl https://www.cloudflare.com/api_json.html \
+          -d a=rec_edit \
+          -d tkn=$cfkey \
+          -d email=$cfuser \
+          -d z=$domain \
+          -d id=$cfid \
+          -d type=A \
+          -d name=$cfhost \
+          -d ttl=1 \
+          -d "content=$WAN_IP" >> $log
+fi
